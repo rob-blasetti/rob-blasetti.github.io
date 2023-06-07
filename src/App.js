5530d4bbd17277533b5c418bd2b70ef36ec5f469
Rob Blasetti <rblasett@thoughtworks.com>
2023-06-07 12:34:57 +1000

    initial create react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Liquid Gold Crypto News Show</h1>
+        <p>Your number one source for all crypto news!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
